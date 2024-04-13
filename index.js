0836894a8707003da020204de28548712983e44b
jasnmar <jasnmar@gmail.com>
2024-04-13 15:53:33 -0400

    Draw Cards

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-
+let deckId
 
 const getDeckBtn = document.getElementById("new-deck")
+const drawCardsBtn = document.getElementById("draw-cards")
 
 getDeckBtn.addEventListener("click", getDeck)
+drawCardsBtn.addEventListener("click", draw2)
 
 async function getDeck(e) {
     e.preventDefault()
@@ -12,20 +14,67 @@ async function getDeck(e) {
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            const deckId = data.deck_id
+            deckId = data.deck_id
             console.log(deckId)
 
         })
 }
+
+async function draw2() {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`, {
+        method: "GET"
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        const cards = data.cards
+        cards.forEach(card => {
+            console.log(card.value+card.suit)
+        });
+    })
+}
+/*
+{
+    "success": true,
+    "deck_id": "ggwjlsdycsgn",
+    "cards": [
+        {
+            "code": "6D",
+            "image": "https://deckofcardsapi.com/static/img/6D.png",
+            "images": {
+                "svg": "https://deckofcardsapi.com/static/img/6D.svg",
+                "png": "https://deckofcardsapi.com/static/img/6D.png"
+            },
+            "value": "6",
+            "suit": "DIAMONDS"
+        },
+        {
+            "code": "QH",
+            "image": "https://deckofcardsapi.com/static/img/QH.png",
+            "images": {
+                "svg": "https://deckofcardsapi.com/static/img/QH.svg",
+                "png": "https://deckofcardsapi.com/static/img/QH.png"
+            },
+            "value": "QUEEN",
+            "suit": "HEARTS"
+        }
+    ],
+    "remaining": 50
+}
+*/
 /**
  * Challenge
  * 
- * Background:
- * The Deck of Cards API expects us to provide the deck id 
- * of the deck we're playing with so it can remember which
- * cards we've already drawn, how many are remaining in the
- * deck, etc.
+ * Task: Using the saved deckId, draw 2 new cards from the deck
  * 
- * Task: save the deck_id from the returned data to a local
- * variable so we can use it later
+ * Docs for original Deck of Cards API: https://deckofcardsapi.com/#draw-card
+ * BaseUrl you'll use: https://apis.scrimba.com/deckofcards/api/deck/
+ * (that will replace the base url of https://deckofcardsapi.com/api/deck/)
+ * that you'll see in the deck of cards API docs.
+ * 
+ * 1. Create a new button that, when clicked, draws 2 cards from the deckId
+ * you have saved
+ *      Note: you'll need to get a new deck every time you refresh the page,
+ *      since you're only saving your deckId in a local variable right now
+ * 2. Log those 2 cards to the console
  */
