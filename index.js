5072ca92cd092ac6d6bb7bdddf6098984cf0d0c4
jasnmar <jasnmar@gmail.com>
2024-04-13 15:35:44 -0400

    then promise experiment

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-/**
- * Challenge: Define our anonymous callback function as a separate function, then
- * pass it as the 2nd parameter to our `addEventListener`
- */
 
-//I did it this way to start
 
 const getDeckBtn = document.getElementById("new-deck")
 
@@ -18,123 +13,39 @@ async function getDeck(e) {
         .then(data => console.log(data))
 }
 
-/**
- * Challenge: 
- * 
- * Part 1: write a `setTimeout` command. Have it wait for 2000 ms before logging "I finally ran!" to the console
- * 
- * Part 2: Upcoming...
- */
-
-// setTimeout(cLogSomething, 2000);
-
-// function cLogSomething() {
-//     console.log("I finally ran")
-// }
+const promise = fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+console.log(promise)
+const p = promise.then()
+console.log(p)
 
 /**
- * Challenge: 
- * 
- * Part 1: Given the array of objects below, create a new array with the `.filter()` array method that contains only the objects where "hasPet" is true
- * 
- * 
- * Part 2: Move the anonymous in-line function to its own, named function
+ * Mini challenge: Figure out what `promise.then()` returns! 
+ * Save the result to a variable and log it to the console.
  */
-// const people = [
-//     { name: "Jack", hasPet: true, age: 12 },
-//     { name: "Jill", hasPet: false, age: 18 },
-//     { name: "Alice", hasPet: true, age: 22 },
-//     { name: "Bob", hasPet: false, age: 32 },
-// ]
+    // .then(data => console.log(data))
 
-// const petPeople = people.filter(peopleTest)
-
-// function peopleTest(person){
-//     return person.hasPet === true
-// }
-// console.log(petPeople)
-
-// https://www.javascripttutorial.net/javascript-array-filter
+    /* 
+- Promises are in one of 3 states at any given time
+    - Pending
+    - Fulfilled
+    - Rejected
+*/
 
 /**
- * Extra challenge to practice array.filter:
+ * Time to be curious!
  * 
- * Using .filter, create a new array of people who are 18 and older
- * (should be Jill, Alice, and Bob)
+ * What would happen if you didn't return `res.json()` 
+ * from the first .then block?
+ * 
+ * What would the next .then() callback receive as its 
+ * parameter if you returned something totally different??
  */
 
-// function ageTest(person) {
-//     return person.age>17
-// }
-// const adults = people.filter(ageTest)
-// console.log(adults)
-
-
-/**
- * Challenge: 
- * 
- * Write your own `filter` function! Don't worry about adding it to the prototype of arrays or anything.
- * This function should take 2 parameters:
- * 1. The array you want to filter through, and
- * 2. A callback function
- * 
- * Steps for filterArray function logic:
- * 1. Initialize a new, empty array which will be returned at the end of the `filterArray`s operations (Completed ✅)
- * 2. Loop through the array passed as the 1st parameter
- * 3. Inside the loop, call the callback function, passing the individual item you're currently looping over as the argument to your callback function
- * 4. If the callback function returns `true`, push the current item you're iterating on in the loop to the new array. If it returns `false`, don't push it to the array.
- * 5. When the loop is over, return the new array
- */
-
-const people = [
-    { name: "Jack", hasPet: true },
-    { name: "Jill", hasPet: false },
-    { name: "Alice", hasPet: true },
-    { name: "Bob", hasPet: false },
-]
-
-function filterArray(array, callback) {
-    const resultingArray = []
-    // Write your filtering logic here
-    array.forEach(element => {
-        if(callback(element)) {
-            resultingArray.push(element)
-        }
-    });
-
-    return resultingArray
-}
-
-/**
- * Challenge: Use your filter array method!
- * Given the above `people` array, return a new array with only people where `hasPet` is true
- * Note: Remember that your callback function will be given the individual item in the array for a parameter
- */
-
-//const peopleWithPets = filterArray(people, person => person.hasPet)
-//console.log(peopleWithPets)
-
-
-/**
- * Challenge: method chaining!
- * 
- * 1. Select the button in the DOM and add an event listener to it without saving the DOM element as a separate variable. I.e. "chain" the `addEventListener` on after your `getElementById()`(When clicked, log "Clicked" to the console)
- *    - I realize this might feel like busywork, but my intent will make sense soon
- * 
- * 2. Given the array below, chain the `.filter` and `.map` array methods together to turn the array into an array of string email addresses of only the people in the array who voted. Log the array of email addresses to the console
- */
-
-// document.getElementById("new-deck").addEventListener("click", function() {
-//     console.log("Clicked!")
-// })
-
-const voters = [
-    {name: "Joe", email: "joe@joe.com", voted: true},
-    {name: "Jane", email: "jane@jane.com", voted: true},
-    {name: "Bo", email: "bo@bo.com", voted: false},
-    {name: "Bane", email: "bane@bane.com", voted: false}
-]
-
-// Write your code below
-votedString = voters.filter(person => person.voted).map(person => person.email)
-console.log(votedString)
+fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(function(res) {
+        return "res dot jason"
+    })
+    .then(function(element) {
+        console.log(element)
+        
+    })
