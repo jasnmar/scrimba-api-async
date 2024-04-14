bcc83b09b0c08ac25b3461a98bd638fe98ff683c
jasnmar <jasnmar@gmail.com>
2024-04-14 05:51:13 -0400

    Card Placeholders

--- index.js
+++ index.js
@@ -76,13 +76,7 @@ async function draw2() {
 /**
  * Challenge:
  * 
- * Start making this look lots nicer :)
- * 
- * 1. Add a card table background with the img/table.png image provided.
- * 2. Move the draw button to the very bottom of the page, full width
- * 3. Add some button styles. You can use the photo on the slides
- * for one option.
+ * Create a spot in the HTML for the cards to be placed in.
+ * Typical playing cards have a 5:7 ratio (width-to-height).
  */
-
-
 
